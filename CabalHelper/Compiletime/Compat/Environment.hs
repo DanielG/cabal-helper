373a468190a393fa2f7cf3851f1050dc6bf5bea1
Daniel Gröber <dxld@darkboxed.org>
2017-09-19 22:30:44 +0200

    Fix CH.C.C.Environment on Prelude without '<$>'

--- CabalHelper/Compiletime/Compat/Environment.hs
+++ CabalHelper/Compiletime/Compat/Environment.hs
@@ -3,4 +3,4 @@ module CabalHelper.Compiletime.Compat.Environment where
 import System.Environment
 
 lookupEnv :: String -> IO (Maybe String)
-lookupEnv var = lookup var <$> getEnvironment
+lookupEnv var = do env <- getEnvironment; return (lookup var env)
