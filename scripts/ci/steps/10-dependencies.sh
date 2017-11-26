16f42e85a064d690f05459306f6b1ea0f70c9ab3
Daniel Gröber <dxld@darkboxed.org>
2017-11-26 19:34:23 +0100

    ci: Add retry loop to 'cabal update'

--- scripts/ci/steps/10-dependencies.sh
+++ scripts/ci/steps/10-dependencies.sh
@@ -1,3 +1,3 @@
-cabal update
+"$CI_SCRIPTS_DIR"/retry.sh  cabal update
 cabal --sandbox-config="$sandbox_config" sandbox init --sandbox="$sandbox"
 cabal --sandbox-config="$sandbox_config" install --only-dependencies
