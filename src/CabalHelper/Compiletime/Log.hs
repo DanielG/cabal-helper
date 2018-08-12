8d7455c4a98d745fbe6291424b7d5f7c4e32559f
Daniel Gröber <dxld@darkboxed.org>
2018-08-13 00:05:25 +0200

    Downgrade license from AGPLv3 to regular GPLv3
    There are only 8 commits in cabal-helper not by me or alanz, see:
    
        $ git log --author='Daniel Gröber\|Alan Zimmerman' --invert-grep -p
        9142d8a9e6ed18faf17a360521fbbbd25f6a3b47
    
    Alan Zimmerman has agreed to the license change on IRC.
    
    The rest are IMO uncopyrightable. I'm pretty sure version-bounds are "facts"
    which are inherently uncopyrightable, i.e. the software builds with this and
    that version, that's a fact. Simple build fixes such as ae568d10 and eb178a5b
    shouldn't really cross the necessary creative threshold.

--- src/CabalHelper/Compiletime/Log.hs
+++ src/CabalHelper/Compiletime/Log.hs
@@ -1,16 +1,16 @@
--- Copyright (C) 2017  Daniel Gröber <dxld ÄT darkboxed DOT org>
+-- Copyright (C) 2017-2018  Daniel Gröber <dxld ÄT darkboxed DOT org>
 --
 -- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Affero General Public License as published by
+-- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 --
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU Affero General Public License for more details.
+-- GNU General Public License for more details.
 --
--- You should have received a copy of the GNU Affero General Public License
+-- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -18,7 +18,7 @@
 {-|
 Module      : CabalHelper.Compiletime.Log
 Description : Basic logging facilities
-License     : AGPL-3
+License     : GPL-3
 -}
 
 module CabalHelper.Compiletime.Log where
