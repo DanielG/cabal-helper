386a42188acd5fb0621490caa5bf6fcb7478fbcf
Daniel Gröber <dxld@darkboxed.org>
2019-01-17 18:31:12 +0100

    Backport new-build CI scripts from master

--- scripts/ci/steps/20-sdist.sh
+++ scripts/ci/steps/20-sdist.sh
@@ -1,6 +1,14 @@
 mkdir -p "$source_dir"
 mkdir -p "$build_dir"
 
-cabal --sandbox-config="$sandbox_config" sdist --builddir="$build_dir" --output-directory="$source_dir"
+cabal act-as-setup -- sdist --output-directory="$source_dir"
+cp cabal.project "$source_dir"/cabal.project
+
+if [ -d cabal-plan/ ]; then
+        (
+            cd cabal-plan/
+            cabal act-as-setup -- sdist --output-directory="$source_dir/cabal-plan"
+        )
+fi
 
 cd "$source_dir"
--- scripts/ci/steps/30-build.sh
+++ scripts/ci/steps/30-build.sh
@@ -1,4 +1,4 @@
 # -fdev enables building the helper "main" exe directly and enables more warnings
-cabal --sandbox-config="$sandbox_config" configure --builddir="$build_dir" --enable-tests -fdev
-cabal --sandbox-config="$sandbox_config" build     --builddir="$build_dir"
-cabal --sandbox-config="$sandbox_config" haddock   --builddir="$build_dir"
+cabal new-configure --builddir="$build_dir" -fdev --enable-tests
+cabal new-build     --builddir="$build_dir"
+cabal new-haddock   --builddir="$build_dir"
