43e779bf6b053825b58deee9d81d29e5faff00de
Luke Lau <luke_lau@icloud.com>
2019-11-17 01:36:18 +0000

    Always include cabal v2 project in discover

--- lib/Distribution/Helper/Discover.hs
+++ lib/Distribution/Helper/Discover.hs
@@ -34,7 +34,6 @@ import System.FilePath
 
 import CabalHelper.Compiletime.Types
 import CabalHelper.Compiletime.Cabal
-import CabalHelper.Compiletime.Program.CabalInstall
 
 -- | @findProjects dir@. Find available project instances in @dir@.
 --
@@ -57,12 +56,8 @@ findProjects dir = execWriterT $ do
   whenM (liftIO $ doesFileExist stackYaml) $
     tell [Ex $ ProjLocStackYaml stackYaml]
 
-  cabalVersion <- liftIO $ let ?verbose = (<=0)
-                               ?progs = defaultPrograms
-                             in cabalInstallVersion
-  when (cabalInstallVer cabalVersion >= makeVersion [3,0,0,0]) $ void $
-    join $ traverse (tell . pure . Ex . ProjLocV2Dir . takeDirectory) <$>
-      liftIO (findCabalFile dir)
+  join $ traverse (tell . pure . Ex . ProjLocV2Dir . takeDirectory) <$>
+    liftIO (findCabalFile dir)
 
   join $ traverse (tell . pure . Ex . ProjLocV1Dir . takeDirectory) <$>
     liftIO (findCabalFile dir)
